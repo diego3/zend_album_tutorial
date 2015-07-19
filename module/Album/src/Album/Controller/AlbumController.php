182cdf4847a84e4506a28d9b0dc70a08fe10a6d9
Diego rosa dos santos <www.diegosantos.com.br@gmail.com>
2015-07-19 04:08:22 -0300

    - database completed

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -6,8 +6,13 @@ namespace Album\Controller;
 
  class AlbumController extends AbstractActionController
  {
-    public function indexAction() {
 	
+	protected $albumTable;
+ 
+    public function indexAction() {
+		return new ViewModel(array(
+             'albums' => $this->getAlbumTable()->fetchAll(),
+        ));
     }
 
     public function addAction() {
@@ -21,4 +26,13 @@ namespace Album\Controller;
     public function deleteAction() {
 	
     }
+	
+	 public function getAlbumTable()
+     {
+         if (!$this->albumTable) {
+             $sm = $this->getServiceLocator();
+             $this->albumTable = $sm->get('Album\Model\AlbumTable');
+         }
+         return $this->albumTable;
+     }
  }
